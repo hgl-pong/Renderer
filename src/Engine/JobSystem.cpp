69ff8c2425a266e2df659ebadd06de4a4f5f9194
hgl <2319100642@qq.com>
2024-03-25 18:56:57 +0800

    worker

--- src/Engine/JobSystem.cpp
+++ src/Engine/JobSystem.cpp
@@ -16,149 +16,104 @@ private:
 
 JobSystem::JobSystem()
 {
-    for (int i = 0; i < m_MaxNumOfWorkers / 2; i++)
-        m_Workers.push_back(std::make_unique<Worker>());
-    HLOG_INFO("Device Supported threads: %d\n", m_MaxNumOfWorkers);
-    HLOG_INFO("JobSystem initialized, number of workers: %d\n", m_Workers.size());
+    // for (int i = 0; i < m_MaxNumOfWorkers / 2; i++)
+    //     m_Workers.push_back(std::make_unique<Worker>());
+    // HLOG_INFO("Device Supported threads: %d\n", m_MaxNumOfWorkers);
+    // HLOG_INFO("JobSystem initialized, number of workers: %d\n", m_Workers.size());
 }
 
 JobSystem::~JobSystem()
 {
-    std::unique_lock<std::mutex> lock(m_JobMutex);
-    jobSystemCV.wait(lock, [&]
-                     { return m_Jobs.empty() && IsFinished(); });
-    for (auto &worker : m_Workers)
-    {
-        worker.reset();
-    }
-    m_Workers.clear();
-    jobSystemSingleton = nullptr;
-    HLOG_INFO("JobSystem destroyed\n");
+    // std::unique_lock<std::mutex> lock(m_JobMutex);
+    // jobSystemCV.wait(lock, [&]
+    //                  { return m_Jobs.empty() && IsFinished(); });
+    // for (auto &worker : m_Workers)
+    // {
+    //     worker.reset();
+    // }
+    // m_Workers.clear();
+    // jobSystemSingleton = nullptr;
+    // HLOG_INFO("JobSystem destroyed\n");
 }
 
 uint32_t JobSystem::RequestWorker()
 {
-    uint32_t id = static_cast<uint32_t>(m_Workers.size());
-    if (id >= m_MaxNumOfWorkers)
-    {
-        HLOG_ERROR("No available worker\n");
-        return UINT_MAX;
-    }
-    m_Workers.push_back(std::make_unique<Worker>(id));
-    HLOG_INFO("Worker requested\n");
-    return id;
+    // uint32_t id = static_cast<uint32_t>(m_Workers.size());
+    // if (id >= m_MaxNumOfWorkers)
+    // {
+    //     HLOG_ERROR("No available worker\n");
+    //     return UINT_MAX;
+    // }
+    // m_Workers.push_back(std::make_unique<Worker>(id));
+    // HLOG_INFO("Worker requested\n");
+    return 0;
 }
 
 bool JobSystem::ReleaseWorker(uint32_t workerId)
 {
-    for (auto it = m_Workers.begin(); it != m_Workers.end(); it++)
-    {
-        if ((*it)->GetWorkerID() == workerId)
-        {
-            m_Workers.erase(it);
-            HLOG_INFO("Worker released\n");
-            return true;
-        }
-    }
-    HLOG_ERROR("Not Found worker\n");
+    // for (auto it = m_Workers.begin(); it != m_Workers.end(); it++)
+    // {
+    //     if ((*it)->GetWorkerID() == workerId)
+    //     {
+    //         m_Workers.erase(it);
+    //         HLOG_INFO("Worker released\n");
+    //         return true;
+    //     }
+    // }
+    // HLOG_ERROR("Not Found worker\n");
     return false;
 }
 
 bool JobSystem::SubmitJob(std::unique_ptr<Job> &job)
 {
-    std::lock_guard<std::mutex> guard(m_JobMutex);
-    if (job == nullptr)
-    {
-        HLOG_ERROR("Submitting a null job\n");
-        return false;
-    }
-    m_Jobs.push_back(std::move(job));
-    m_JobCV.notify_all();
+    // std::lock_guard<std::mutex> guard(m_JobMutex);
+    // if (job == nullptr)
+    // {
+    //     HLOG_ERROR("Submitting a null job\n");
+    //     return false;
+    // }
+    // m_Jobs.push_back(std::move(job));
+    // m_JobCV.notify_all();
     return true;
 }
 
 bool JobSystem::Run()
 {
-    for (auto &worker : m_Workers)
-    {
-        worker->Activate();
-    }
+    // for (auto &worker : m_Workers)
+    // {
+    //     worker->Activate();
+    // }
     return true;
 }
 
 bool JobSystem::IsFinished() const
 {
-    for (auto &worker : m_Workers)
-    {
-        if (!worker->IsBusy())
-        {
-            return false;
-        }
-    }
+    // for (auto &worker : m_Workers)
+    // {
+    //     if (!worker->IsBusy())
+    //     {
+    //         return false;
+    //     }
+    // }
     return true;
 }
 
 void JobSystem::SortJobs(ScheduleStrategy strategy)
 {
-    std::lock_guard<std::mutex> guard(m_JobMutex);
-    switch (strategy)
-    {
-    case ScheduleStrategy::FIFO:
-        break;
-    case ScheduleStrategy::LIFO:
-        std::reverse(m_Jobs.begin(), m_Jobs.end());
-        break;
-    case ScheduleStrategy::PRIORITY:
-        std::sort(m_Jobs.begin(), m_Jobs.end());
-        break;
-    default:
-        break;
-    }
-}
-
-bool JobSystem::GetFirstJob(std::unique_ptr<Job> &job)
-{
-    std::lock_guard<std::mutex> guard(m_JobMutex);
-    if (m_Jobs.empty())
-    {
-        m_JobCV.notify_all();
-        return false;
-    }
-    job = std::move(m_Jobs.front());
-    m_Jobs.pop_front();
-    return true;
-}
-
-bool JobSystem::GetFirstJob(std::unique_ptr<Job> &job, uint32_t workerID)
-{
-    std::lock_guard<std::mutex> guard(m_JobMutex);
-    if (m_Jobs.empty())
-    {
-        m_JobCV.notify_all();
-        return false;
-    }
-    std::deque<std::unique_ptr<Job>> tempQueue;
-    bool result = false;
-    while (!m_Jobs.empty())
-    {
-        if (m_Jobs.front()->GetWorkerID() == workerID)
-        {
-            job = std::move(m_Jobs.front());
-            m_Jobs.pop_front();
-            result = true;
-            break;
-        }
-        tempQueue.push_back(std::move(m_Jobs.front()));
-        m_Jobs.pop_front();
-    }
-
-    while (!m_Jobs.empty())
-    {
-        tempQueue.push_back(std::move(m_Jobs.front()));
-        m_Jobs.pop_front();
-    }
-    m_Jobs = std::move(tempQueue);
-    return result;
+    // std::lock_guard<std::mutex> guard(m_JobMutex);
+    // switch (strategy)
+    // {
+    // case ScheduleStrategy::FIFO:
+    //     break;
+    // case ScheduleStrategy::LIFO:
+    //     std::reverse(m_Jobs.begin(), m_Jobs.end());
+    //     break;
+    // case ScheduleStrategy::PRIORITY:
+    //     std::sort(m_Jobs.begin(), m_Jobs.end());
+    //     break;
+    // default:
+    //     break;
+    // }
 }
 
 JobSystem *JobSystem::CreateJobSystem()
