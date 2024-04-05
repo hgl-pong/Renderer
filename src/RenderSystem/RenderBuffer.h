79c5777ed2e5fa20d8397eeba73f3280317496ae
heguoling <2319100642@qq.com>
2024-04-05 23:44:30 +0800

    render buffer

--- src/RenderSystem/RenderBuffer.h
+++ src/RenderSystem/RenderBuffer.h
@@ -26,25 +26,6 @@ public:
 	void Init() override {
 				// Create buffer
 		VkDeviceSize bufferSize = sizeof(DataBlock) * m_Data.size();
-		_CreateBuffer(bufferSize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, m_Buffer, m_BufferMemory);
-
-		// Copy data
-		void* data;
-		vkMapMemory(m_Device, m_BufferMemory, 0, bufferSize, 0, &data);
-		memcpy(data, m_Data.data(), (size_t)bufferSize);
-		vkUnmapMemory(m_Device, m_BufferMemory);
-
-		// Set binding description
-		m_BindingDescription.binding = 0;
-		m_BindingDescription.stride = sizeof(DataBlock);
-		m_BindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-
-		// Set attribute descriptions
-		m_AttributeDescriptions.resize(1);
-		m_AttributeDescriptions[0].binding = 0;
-		m_AttributeDescriptions[0].location = 0;
-		m_AttributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
-		m_AttributeDescriptions[0].offset = offsetof(DataBlock, position);	
 	};
 
 private:
